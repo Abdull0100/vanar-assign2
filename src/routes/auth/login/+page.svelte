697f98933b7f37152e6d55fbe34319f09ba59744
Abdullah Tahir <abdullahtahir786241@gmail.com>
2025-08-13 14:08:31 +0500

    added back button to login and signup

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import LoginForm from "$lib/components/login-form.svelte";
+    import BackButton from "$lib/components/back-button.svelte";
 </script>
 
 <svelte:head>
     <title>Login</title>
 </svelte:head>
 
-<div class="flex h-screen w-full items-center justify-center px-4">
+<div class="relative flex h-screen w-full items-center justify-center px-4">
+    <BackButton href="/" label="Back to homepage" />
     <LoginForm />
-</div>``
+</div>
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import SignupForm from "$lib/components/signup-form.svelte";
+	import BackButton from "$lib/components/back-button.svelte";
 </script>
 
 <svelte:head>
 	<title>Sign-up</title>
 </svelte:head>	
 
-<div class="flex h-screen w-full items-center justify-center px-4">
+<div class="relative flex h-screen w-full items-center justify-center px-4">
+	<BackButton href="/" label="Back to homepage" />
 	<SignupForm />
 </div>
